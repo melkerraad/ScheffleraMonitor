24b08c5a813a6dd4f6591b03a593e95b2c249488
melkerr1 <melkerr@chalmers.se>
2025-06-10 18:48:49 +0200

    Added printed alerts for poor temp and humidity

--- check.py
+++ check.py
@@ -7,6 +7,10 @@ def check_threshold(measurement, type):
         elif target.acceptable_temperature[0] <= measurement <= target.acceptable_temperature[1]:
             return "acceptable"
         else:
+            if target.acceptable_temperature[0] > measurement:
+                print("Plant too cold!")
+            elif measurement > target.acceptable_temperature[1]:
+                print("Plant too hot!")
             return "poor"
 
     if type == "humidity":
@@ -15,4 +19,8 @@ def check_threshold(measurement, type):
         elif target.acceptable_humidity[0] <= measurement <= target.acceptable_humidity[1]:
             return "acceptable"
         else:
+            if target.acceptable_humidity[0] > measurement:
+                print("Air humidity too low!")
+            elif measurement > target.acceptable_temperature[1]:
+                print("Air humidity too high!")
             return "poor"
