99a7e3bc40b26fc6f1e29317955a34a73682570c
melkerr1 <melkerr@chalmers.se>
2025-07-05 06:21:57 +0200

    moved url and token to keys.py

--- keys.py
+++ keys.py
@@ -1,7 +1,5 @@
 WIFI_SSID = 'WIFIHUB_690852'
 WIFI_PASS = '37y5uw6b'
 
-influx_token="NDXGmGdgj3oMAOsFP73pkSP4GPKWoOOngBIbiNn8LYGVNMncj254xATMLI_e4b4FU8N0hFpmn-KxKL7V6EpQ8g=="
-influx_url="http://192.168.0.47:8086/api/v2/write?org=Home_System&bucket=First_bucket&precision=s"
-
-#
+influx_token = "uxJhhOdZDRqtAs7E7JWmjzjiz4kCttpgh0rUMH28mpqsnFRJtl7XXZ4QcQuTljIo6YB7fiVq_iw0g5KuSI8tKQ=="
+influx_url = "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=IoT&bucket=data&precision=s"
--- write_DB.py
+++ write_DB.py
@@ -3,8 +3,8 @@ import urequests as requests
 import time
 import keys
 
-influx_token = "uxJhhOdZDRqtAs7E7JWmjzjiz4kCttpgh0rUMH28mpqsnFRJtl7XXZ4QcQuTljIo6YB7fiVq_iw0g5KuSI8tKQ=="
-influx_url = "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=IoT&bucket=data&precision=s"
+influx_token = keys.influx_token
+influx_url = keys.influx_url
 
 
 
